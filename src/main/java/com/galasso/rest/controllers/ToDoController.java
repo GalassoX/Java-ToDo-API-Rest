3851759b171070464592013bcb90b3beef4d4d34
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-02-02 22:28:44 -0500

    add corsW

--- src/main/java/com/galasso/rest/controllers/ToDoController.java
+++ src/main/java/com/galasso/rest/controllers/ToDoController.java
@@ -7,7 +7,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
